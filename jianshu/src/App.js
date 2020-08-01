f237d59cb726fa19ebbd91481716577611ac9213
Fordkuiyang <yk804031@126.com>
2020-08-01 22:00:10 +0800

    learn keyframes in css

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -1,8 +1,27 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { CSSTransition } from "react-transition-group";
+import "./style.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true,
+    };
+  }
+  handleToggle = () => {
+    this.setState({
+      show: this.state.show ? false : true,
+    });
+  };
   render() {
-    return <div>hello ford k yang</div>;
+    return (
+      <Fragment>
+        <div>hello, man</div>
+
+        <button onClick={this.handleToggle}>toggle</button>
+      </Fragment>
+    );
   }
 }
 
